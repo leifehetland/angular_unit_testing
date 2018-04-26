876875abccbd2d03039e17a635fd0f46c161905f
Leif Hetland <lehetland1@gmail.com>
2018-04-26 16:36:17 -0500

    testing with strings and arrays

--- src/app/compute/compute.spec.ts
+++ src/app/compute/compute.spec.ts
@@ -1,12 +1,12 @@
-import { compute } from "./compute";
+import { compute } from './compute';
 
 describe('compute', () => {
-  it('Should return 0 if input is negative.', () => {
+  it('should return 0 if input is negative.', () => {
     const result = compute(-1);
     expect(result).toBe(0);
   })
 
-  it('Should increment the input if it is positive.', () => {
+  it('should increment the input if it is positive.', () => {
     const result = compute(1);
     expect(result).toBe(2);
   })
