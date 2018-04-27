0cd692dd24aa25aeba11a4b25de859a8c2c5201e
Leif Hetland <lehetland1@gmail.com>
2018-04-27 12:24:41 -0500

    confirmation boxes

--- src/app/services/todo.component.spec.ts
+++ src/app/services/todo.component.spec.ts
@@ -59,4 +59,24 @@ describe('TodoComponent', () => {
 
     expect(component.message).toBe(error);
   });
+
+  it('should call the server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should NOT call the server to delete a todo if user cancels ', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
+    expect(spy).not.toHaveBeenCalledWith(1);
+  });
 });
